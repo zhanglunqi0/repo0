623a34c7cec3e02fbca91d200b917a7ece9fc1ae
Alexandre Sablayrolles <asablayrolles@fb.com>
2021-10-06 14:09:20 +0200

    Make collate handle more cases

--- opacus/data_loader.py
+++ opacus/data_loader.py
@@ -6,43 +6,58 @@ from opacus.utils.uniform_sampler import UniformWithReplacementSampler
 from torch.utils.data._utils.collate import default_collate
 
 
-def wrap_collate_with_empty(collate_fn: Optional[_collate_fn_t], sample_empty_shapes: Sequence):
-    #TODO: does it work with padding and packed sequences?
+def wrap_collate_with_empty(
+    collate_fn: Optional[_collate_fn_t], sample_empty_shapes: Sequence
+):
     def collate(batch):
         if len(batch) > 0:
             return collate_fn(batch)
         else:
-            return [torch.zeros(x) for x in sample_empty_shapes]
+            if type(sample_empty_shapes) is collections.abc.Sequence:
+                return [torch.zeros(x) for x in sample_empty_shapes]
+            else:
+                return torch.zerso(sample_empty_shapes)
 
     return collate
 
 
-def shape_safe(x):
-    if hasattr(x, "shape"):
-        return x.shape
-    else:
-        return ()
 
 
 class DPDataLoader(DataLoader):
-
-    def __init__(self, dataset: Dataset, sample_rate: float,
-                 num_workers: int = 0, collate_fn: Optional[_collate_fn_t] = None,
-                 pin_memory: bool = False, drop_last: bool = False,
-                 timeout: float = 0, worker_init_fn: Optional[_worker_init_fn_t] = None,
-                 multiprocessing_context=None, generator=None,
-                 *, prefetch_factor: int = 2,
-                 persistent_workers: bool = False):
+    def __init__(
+        self,
+        dataset: Dataset,
+        sample_rate: float,
+        num_workers: int = 0,
+        collate_fn: Optional[_collate_fn_t] = None,
+        pin_memory: bool = False,
+        drop_last: bool = False,
+        timeout: float = 0,
+        worker_init_fn: Optional[_worker_init_fn_t] = None,
+        multiprocessing_context=None,
+        generator=None,
+        *,
+        prefetch_factor: int = 2,
+        persistent_workers: bool = False
+    ):
 
         self.sample_rate = sample_rate
         batch_sampler = UniformWithReplacementSampler(
-            num_samples=len(dataset), # type: ignore[assignment, arg-type]
+            num_samples=len(dataset),  # type: ignore[assignment, arg-type]
             sample_rate=sample_rate,
             generator=generator,
         )
-        sample_empty_shapes = [[0, *shape_safe(x)] for x in dataset[0]]
+        
+
         if collate_fn is None:
             collate_fn = default_collate
+        collated_data = collate_fn([dataset[0]])
+
+        # Opacus empty batches work for Tensors and sequences of Tensors
+        if type(collated_data) is torch.Tensor:
+            sample_empty_shapes = [0, collated_data.shape[1:]]
+        elif type(collated_data) is collections.abc.Sequence and all([type(x) is torch.Tensor for x in collated_data]):
+            sample_empty_shapes = [[0, x.shape[1:]] for x in collated_data]
 
         super().__init__(
             dataset=dataset,
@@ -66,7 +81,7 @@ class DPDataLoader(DataLoader):
 
         return cls(
             dataset=data_loader.dataset,
-            sample_rate=1/len(data_loader),
+            sample_rate=1 / len(data_loader),
             num_workers=data_loader.num_workers,
             pin_memory=data_loader.pin_memory,
             drop_last=data_loader.drop_last,
@@ -75,5 +90,5 @@ class DPDataLoader(DataLoader):
             multiprocessing_context=data_loader.multiprocessing_context,
             generator=data_loader.generator,
             prefetch_factor=data_loader.prefetch_factor,
-            persistent_workers=data_loader.persistent_workers
+            persistent_workers=data_loader.persistent_workers,
         )
